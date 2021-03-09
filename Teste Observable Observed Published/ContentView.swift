99e0cc85bc5cd69561504cf0ac5afc8be84978ae
Edivaldo Goncalves <edivaldo.goncalves@chuva.io>
2021-03-09 10:04:22 -0100

    Initial Commit

---
+++ Teste Observable Observed Published/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Teste Observable Observed Published
+//
+//  Created by Edivaldo Goncalves on 3/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Teste Observable Observed Published/Teste_Observable_Observed_PublishedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Teste_Observable_Observed_PublishedApp.swift
+//  Teste Observable Observed Published
+//
+//  Created by Edivaldo Goncalves on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Teste_Observable_Observed_PublishedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
